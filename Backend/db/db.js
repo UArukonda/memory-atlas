9239e267772ffa81362d86307d90b385c590b5d5
UArukonda <109522168+UArukonda@users.noreply.github.com>
2026-08-09 17:44:41 +0530

    remove unnecessary console.log

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const connectDB = async (connectionString) => {
   try {
     await mongoose.connect(connectionString);
-    console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
     process.exit(1);
