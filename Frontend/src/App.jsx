91153b50ceb70f46771f2dfdfac1c1ac03884f13
UArukonda <109522168+UArukonda@users.noreply.github.com>
2026-08-09 23:19:22 +0530

    setup routing and styles for signup and login pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
