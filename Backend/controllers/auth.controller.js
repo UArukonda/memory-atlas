6f9f8985574fc0ccb23b8f46e6da782035009b40
UArukonda <109522168+UArukonda@users.noreply.github.com>
2026-08-09 17:47:56 +0530

    write logic to handle login and create jwt token

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -3,36 +3,86 @@ const {
   findUserByEmail,
   findUserByUsername,
   createUser,
-} = require("../models/auth.model.js");
+} = require("../models/user.model.js");
+const jwt = require("jsonwebtoken");
 
-function registerUser(req, res) {
-  // Duplicate email
-  const { username, email, password } = req.body;
-  findUserByEmail(email)
-    .then((existingEmail) => {
-      if (existingEmail) {
-        return res.status(409).send({
-          message: "user with this email already exists, please login",
-        });
-      }
-      return findUserByUsername(username);
-    })
-    .then((existingUsername) => {
-      if (existingUsername) {
-        return res.status(409).send({
-          message: "username already taken, please try different username",
-        });
-      }
+async function registerUser(req, res, next) {
+  try {
+    const { username, email, password } = req.body;
 
-      return bcrypt.hash(password, 10);
-    })
-    .then((hashedPassword) => {
-      return createUser({ username, email, password: hashedPassword });
-    })
-    .then((user) => {
-      return res.status(201).send({ id: user._id, email: user.email });
-    })
-    .catch((err) => next(err));
+    const existingEmail = await findUserByEmail(email);
+
+    if (existingEmail) {
+      return res.status(409).send({
+        message: "user with this email already exists, please login",
+      });
+    }
+
+    const existingUsername = await findUserByUsername(username);
+
+    if (existingUsername) {
+      return res.status(409).send({
+        message: "username already taken, please try different username",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await createUser({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).send({
+      id: newUser._id,
+      email: newUser.email,
+      message: "Registration successful. Please log in.",
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = { registerUser };
+async function loginUser(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await findUserByEmail(email);
+
+    if (!existingUser) {
+      return res.status(404).send({
+        message: "Invalid email or password.",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(401).send({
+        message: "Invalid email or password.",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        email: existingUser.email,
+        username: existingUser.username,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    return res.status(200).send({
+      token,
+      user: {
+        email: existingUser.email,
+        id: existingUser._id,
+        username: existingUser.username,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { registerUser, loginUser };
