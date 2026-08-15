2de6c8529689a83660be6f525c4be03df0e52703
UArukonda <109522168+UArukonda@users.noreply.github.com>
2026-08-15 17:31:06 +0530

    write updateUserData and deleteUser, to modify documents

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -17,9 +17,20 @@ function comparePassword(user, pwd) {
   return user.comparePassword(pwd);
 }
 
+function updateUserData(user, key, value) {
+  user[key] = value;
+  return user.save();
+}
+
+function deleteUserByEmail(email) {
+  return User.deleteOne({ email });
+}
+
 module.exports = {
   findUserByEmail,
   findUserByUsername,
   createUser,
   comparePassword,
+  updateUserData,
+  deleteUserByEmail,
 };
