63af2d002239f7b6a4212b076ee5390b367039b1
UArukonda <109522168+UArukonda@users.noreply.github.com>
2026-08-15 17:25:43 +0530

    implements tests to reset password and delete user

--- Backend/__tests__/user.test.js
+++ Backend/__tests__/user.test.js
@@ -2,7 +2,7 @@ require("./setup.js");
 const request = require("supertest");
 const app = require("../app.js");
 
-describe("GET /api/users/me", () => {
+xdescribe("GET /api/users/me", () => {
   test("should return the user", () => {
     const agent = request.agent(app);
 
@@ -32,5 +32,38 @@ describe("GET /api/users/me", () => {
           avatar: expect.any(String),
         }),
       );
+  });
+});
+
+describe("DELETE /api/users/me", () => {
+  test("Should delete the authenticated user", () => {
+    const agent = request.agent(app);
+
+    return agent
+      .post("/api/auth/register")
+      .send({
+        username: "uarukonda",
+        email: "uarukonda@gmail.com",
+        password: "12121212",
+      })
+      .expect(201)
+      .then(() => {
+        return agent
+          .post("/api/auth/login")
+          .send({
+            email: "uarukonda@gmail.com",
+            password: "12121212",
+          })
+          .expect(200);
+      })
+      .then(() => {
+        return agent.delete("/api/users/me").expect(200);
+      })
+      .then(() => {
+        return agent.get("/api/users/me").expect(404);
+      })
+      .then((response) => {
+        expect(response.body).toEqual({ message: "User not found" });
+      });
   });
 });
